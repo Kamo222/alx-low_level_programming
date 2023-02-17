a0a228db47eecfefa893ed71b0c6cdbe56cd0d90
Kamohelo Mamaregane <kamohelo.mamaregane1@gmail.com>
2023-02-17 12:30:09 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all numbers of base 10 from 0
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int number = 0;
+
+while (number <= 9)
+{
+	printf("%d", number);
+	++number;
+}
+return (0);
+}
