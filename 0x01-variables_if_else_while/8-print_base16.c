a9a918a3943010338c84d2ba16b5b9876cf36d5f
Kamohelo Mamaregane <kamohelo.mamaregane1@gmail.com>
2023-02-17 13:17:46 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all digits of base 16 from 0
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int number = 0;
+char alphabet = 'A';
+
+while (number <= 9)
+{
+	putchar(number + 0);
+	++number;
+}
+while (alphabet <= 'F')
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
