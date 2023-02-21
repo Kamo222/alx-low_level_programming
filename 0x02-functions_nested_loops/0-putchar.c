0d6df50a159da88edfe36f2e5dc1557ee095c009
Kamohelo Mamaregane <kamohelo.mamaregane1@gmail.com>
2023-02-21 13:47:31 -0800

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
- * Description: prints _putchar using putchar prototype
+ * Description: prints _putchar
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("_putchar");
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; ++i)
+		_putchar(str[i]);
+	_putchar('\n');
 
 	return (0);
 }
