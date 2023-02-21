da282b3595326895867b421ed224a9abff833fdf
Kamohelo Mamaregane <kamohelo.mamaregane1@gmail.com>
2023-02-21 13:09:06 -0800

    Initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
